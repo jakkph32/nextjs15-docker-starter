55ce6b0281c17aadae222c8253ec9b519b4c5d83
Jakkraphop Pengchan <jakkph32@gmail.com>
2025-02-04 04:23:24 +0700

    Add custom build ID generation using GIT_HASH in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   cacheHandler: require.resolve('./cache-handler.js'),
   cacheMaxMemorySize: 0,
   output: 'standalone',
+  generateBuildId: async () => {
+    return process.env.GIT_HASH!;
+  },
 };
 
 export default nextConfig;
