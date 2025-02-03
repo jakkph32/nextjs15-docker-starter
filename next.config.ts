4259dbe9f5a8cf6c4ac4ca0c240c220997adb264
Jakkraphop Pengchan <jakkph32@gmail.com>
2025-02-04 04:07:01 +0700

    Remove unused build ID generation from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   cacheHandler: require.resolve('./cache-handler.js'),
   cacheMaxMemorySize: 0,
-  /* config options here */
-  generateBuildId: async () => {
-    // You can, for example, get the latest git commit hash here
-    return process.env.GIT_HASH!;
-  },
-
   output: 'standalone',
 };
 
