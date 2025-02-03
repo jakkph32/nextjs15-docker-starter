b5b018e6cebfa6e2a02497a138cae856d2279a98
Jakkraphop Pengchan <jakkph32@gmail.com>
2025-02-04 05:31:42 +0700

    Update Dockerfiles to use Node.js 22 and install the latest npm alongside corepack

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   cacheHandler: require.resolve('./cache-handler.js'),
   cacheMaxMemorySize: 0,
   output: 'standalone',
-  generateBuildId: async () => {
-    return process.env.GIT_HASH!;
-  },
 };
 
 export default nextConfig;
